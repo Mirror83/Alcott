297165c44c3d461d5e5e11c1ac643b90dcce9158
Mirror83 <glenochieng045@gmail.com>
2023-06-10 18:20:13 +0300

    Registered the database context and product service with the app

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,11 +1,17 @@
+using AlcottBackend.Data;
+using AlcottBackend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSqlite<DatabaseContext>("Data Source=AlcottBackend.db");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<ProductService>();
 
 var app = builder.Build();
 
