c04bd45534b3a390e39fcf66e46142f312afdd61
Mirror83 <glenochieng045@gmail.com>
2023-06-11 16:34:17 +0300

    Added the Sale model

---
+++ backend/Models/Sale.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AlcottBackend.Models;
+
+public class Sale
+{
+    public int Id { get; set; }
+    public decimal AmountPaid { get; set; }
+    public DateTime Time { get; set; } = DateTime.Now;
+    // A number less than 1 but greater than or equal to 0 representing the percentage
+    // that a sale was given
+    public decimal DiscountPercentage { get; set; } = 0;
+
+    [Required]
+    public string? PaymentType { get; set; } = "Cash";
+
+}
