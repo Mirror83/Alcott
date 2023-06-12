65155b23edf3d319d761cd06a0d66222c2d2090e
Mirror83 <glenochieng045@gmail.com>
2023-06-12 12:12:37 +0300

    Added SaleService to the app

--- backend/Program.cs
+++ backend/Program.cs
@@ -21,6 +21,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<ProductService>();
+builder.Services.AddScoped<SaleService>();
 
 var app = builder.Build();
 
