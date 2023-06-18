e1d5441cd2d3ac2e3e9497b24bd986740922a223
Mirror83 <glenochieng045@gmail.com>
2023-06-18 20:48:19 +0300

    Added the Orders navigation property

--- backend/Models/OrderDetail.cs
+++ backend/Models/OrderDetail.cs
@@ -8,4 +8,5 @@ public class OrderDetail
     public int Quantity { get; set; }
     public Product? Product { get; set; }
     public Order? Order { get; set; }
+    public ICollection<OrderDetail>? Orders { get; set; }
 }
