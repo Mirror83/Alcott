9ec0bb9992409a7206b9a7c346dcc1ad2c1b19de
Mirror83 <glenochieng045@gmail.com>
2023-06-21 13:29:46 +0300

    Added default value for ReceivedAt

--- backend/Models/Order.cs
+++ backend/Models/Order.cs
@@ -3,7 +3,7 @@ namespace AlcottBackend.Models;
 public class Order
 {
     public int Id { get; set; }
-    public DateTime ReceivedAt { get; set; }
+    public DateTime ReceivedAt { get; set; } = DateTime.Now;
     public double PricePaid { get; set; }
     public ICollection<OrderDetail>? OrderDetails { get; set; }
 }
