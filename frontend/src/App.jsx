6c5a727c5d7bd8dbd5d17e9d2784fcc615c0d756
imm16 <117066365+imm16@users.noreply.github.com>
2023-06-21 16:10:19 +0300

    added import sales and orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/login";
 import Navbar from "./components/Navbar";
+import Sales from "./pages/sales";
+import Orders from "./pages/orders";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <Home />;
+  return <Sales/>;
 }
 
 export default App;
