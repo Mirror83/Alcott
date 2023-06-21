5a67184bc0739fa570fb3e22c03f6758ef679ba9
Mirror83 <glenochieng045@gmail.com>
2023-06-21 13:26:08 +0300

    Added JsonIgnore attribute to nav properties

--- backend/Models/OrderDetail.cs
+++ backend/Models/OrderDetail.cs
@@ -1,3 +1,5 @@
+using System.Text.Json.Serialization;
+
 namespace AlcottBackend.Models;
 
 public class OrderDetail
@@ -6,7 +8,10 @@ public class OrderDetail
     public int OrderId { get; set; }
     public int ProductId { get; set; }
     public int Quantity { get; set; }
+    [JsonIgnore]
     public Product? Product { get; set; }
+    [JsonIgnore]
     public Order? Order { get; set; }
+    [JsonIgnore]
     public ICollection<OrderDetail>? Orders { get; set; }
 }
