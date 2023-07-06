ed9c6999bea9cac0120ba54cbcd22520f8ca6b54
Mirror83 <glenochieng045@gmail.com>
2023-07-06 21:58:35 +0300

    Add getDailyReport method

--- AlcottBackend/AlcottBackend/Services/ReportService.cs
+++ AlcottBackend/AlcottBackend/Services/ReportService.cs
@@ -1,4 +1,6 @@
+using AlcottBackend.ClientData;
 using AlcottBackend.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace AlcottBackend.Services;
 
@@ -10,6 +12,40 @@ public class ReportService
     public ReportService(DatabaseContext context)
     {
         _context = context;
+    }
+
+    public ReportResponse getDailyReport()
+    {
+        var todaySales = _context.Sales.AsNoTracking().Where(
+            sale => sale.RecordedAt.Date == DateTime.Today.Date
+        ).ToList();
+
+        int productsSold = 0;
+        todaySales.ForEach(sale => productsSold += sale!.SaleDetails!.Count());
+
+        var totalAmountOnSales = todaySales.Sum(sale => sale.AmountPaid);
+
+        var todayOrders = _context.Orders.AsNoTracking().Where(
+            order => order.ReceivedAt.Date == DateTime.Today.Date
+        ).ToList();
+
+        int productsOrdered = 0;
+        todayOrders.ForEach(order => productsOrdered += order!.OrderDetails!.Count());
+
+        var totalAmountOnOrders = todayOrders.Sum(order => order.AmountPaid);
+
+
+        return new ReportResponse
+        {
+            ProductsSold = productsSold,
+            SalesMade = todaySales.Count(),
+            TotalAmountOnSales = totalAmountOnSales,
+            OrdersReceived = todayOrders.Count(),
+            ProductsOrdered = productsOrdered,
+            TotalAmountOnOrders = totalAmountOnOrders
+        };
+
+
     }
 
 
