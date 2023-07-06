1fb358db67052e26ed6ceef91218ff55dae67f9d
Mirror83 <glenochieng045@gmail.com>
2023-07-06 12:15:05 +0300

    Added newly created migration files from when payment method was added to Sale

---
+++ AlcottBackend/AlcottBackend/Migrations/20230706080846_AddPaymentMethodToSale.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlcottBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPaymentMethodToSale : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "PaymentMethod",
+                table: "Sales",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Sales");
+        }
+    }
+}
