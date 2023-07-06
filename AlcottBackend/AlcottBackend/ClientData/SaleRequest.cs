6160cb8f37f82b5d95fcf7d92ba575c8bb9de156
Mirror83 <glenochieng045@gmail.com>
2023-07-06 12:11:54 +0300

    Updated SaleRequest to accept payment method

--- AlcottBackend/AlcottBackend/ClientData/SaleRequest.cs
+++ AlcottBackend/AlcottBackend/ClientData/SaleRequest.cs
@@ -5,6 +5,7 @@ namespace AlcottBackend.ClientData;
 public class SaleRequest
 {
     public decimal AmountPaid { get; set; }
+    public byte PaymentMethod { get; set; }
 
     public required List<IdQuantityPair> SaleDetails { get; set; }
 }
